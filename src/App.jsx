1928982cb62ebf2b6cbbd0ceede8193655d56561
ScriptD <adironaldo1234@gmail.com>
2023-05-26 16:07:36 +0530

    added how-to-use

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Date from "./components/Date";
 export default function App() {
   return (
     <div className="App">
+      <p className="how-to-use">Use minutes, hours, days, years, seconds, milliseconds for duration, example ={">"} 10days, 2years etc</p>
       <Date />
     </div>
   );
